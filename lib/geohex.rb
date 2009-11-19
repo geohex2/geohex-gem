b18150a62bfc8dbde4ea4d3d4da684f5df71238e
Haruyuki SEKI <hal.marsellus@gmail.com>
2009-11-19 14:48:20 +0900

    passed first geohex code

--- lib/geohex.rb
+++ lib/geohex.rb
@@ -1,45 +1,75 @@
-# 
-
+#
+# ported from perl libraries http://svn.coderepos.org/share/lang/perl/Geo-Hex/trunk/lib/Geo/Hex.pm
+# author Haruyuki Seki 
+#
 class GeoHex
+  H_KEY       = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX';
+
   MIN_X_LON   = 122930; #与那国島
   MIN_X_LAT   = 24448;
   MIN_Y_LON   = 141470; ##南硫黄島
   MIN_Y_LAT   = 24228;
+  H_GRID      = 1000;
+  H_SIZE      = 0.5;
   
+
   def self.encode(lat,lon,level=7)
     raise ArgumentError, "latitude must be between -90 and 90" if (lat < -90 || lat > 90)
     raise ArgumentError, "longitude must be between -180 and 180" if (lon < -180 || lon > 180)
     raise ArgumentError, "level must be between 1 and 60" if (level < 1 || level > 60)
     
-#     croak 'Level must be between 1 and 60' if ( $level !~ /^\d+$/ || $level < 1 || $level > 60 );
+    lon_grid = lon * H_GRID
+    lat_grid = lat * H_GRID
+    unit_x   = 6.0  * level * H_SIZE
+    unit_y   = 2.8  * level * H_SIZE
+    h_k      = (( (1.4 / 3) * H_GRID).round.to_f / H_GRID)
 
-#     my $lon_grid = $lon * $h_grid;
-#     my $lat_grid = $lat * $h_grid;
-#     my $unit_x   = 6.0  * $level * $h_size;
-#     my $unit_y   = 2.8  * $level * $h_size;
-#     my $h_k      = ( round( (1.4 / 3) * $h_grid) ) / $h_grid;
-#     my $base_x   = floor( ($min_x_lon + $min_x_lat / $h_k      ) / $unit_x);
-#     my $base_y   = floor( ($min_y_lat - $h_k       * $min_y_lon) / $unit_y);
-#     my $h_pos_x  = ( $lon_grid + $lat_grid / $h_k     ) / $unit_x - $base_x;
-#     my $h_pos_y  = ( $lat_grid - $h_k      * $lon_grid) / $unit_y - $base_y;
-#     my $h_x_0    = floor($h_pos_x);
-#     my $h_y_0    = floor($h_pos_y);
-#     my $h_x_q    = floor(($h_pos_x - $h_x_0) * 100) / 100;
-#     my $h_y_q    = floor(($h_pos_y - $h_y_0) * 100) / 100;
-#     my $h_x      = round($h_pos_x);
-#     my $h_y      = round($h_pos_y);
-#     if ( $h_y_q > -$h_x_q + 1 ) {
-#         if( ($h_y_q < 2 * $h_x_q ) && ( $h_y_q > 0.5 * $h_x_q ) ){
-#             $h_x = $h_x_0 + 1;
-#             $h_y = $h_y_0 + 1;
-#        }
-#     } elsif ( $h_y_q < -$h_x_q + 1 ) {
-#         if( ($h_y_q > (2 * $h_x_q ) - 1 ) && ( $h_y_q < ( 0.5 * $h_x_q ) + 0.5 ) ) {
-#             $h_x = $h_x_0;
-#             $h_y = $h_y_0;
-#         }
-#     }
+    base_x   = ( (MIN_X_LON + MIN_X_LAT / h_k      ) / unit_x).floor
+    base_y   = ( (MIN_Y_LAT - h_k      * MIN_Y_LON) / unit_y).floor
+    h_pos_x  = ( lon_grid + lat_grid / h_k     ) / unit_x - base_x
+    h_pos_y  = ( lat_grid - h_k      * lon_grid) / unit_y - base_y
+    h_x_0    = h_pos_x.floor
+    h_y_0    = h_pos_y.floor
+    h_x_q    = ((h_pos_x - h_x_0) * 100).floor / 100
+    h_y_q    = ((h_pos_y - h_y_0) * 100).floor / 100
+    h_x      = h_pos_x.round
+    h_y      = h_pos_y.round
 
-#     return __hyhx2geohex( $h_y, $h_x, $level );
+      
+    if ( h_y_q > -h_x_q + 1 ) 
+      
+      if ( h_y_q < (2 * h_x_q ) and  h_y_q > (0.5 * h_x_q ) )
+        h_x = h_x_0 + 1
+        h_y = h_y_0 + 1
+      end
+    elsif ( h_y_q < -h_x_q + 1 ) 
+      if( (h_y_q > (2 * h_x_q ) - 1 ) && ( h_y_q < ( 0.5 * h_x_q ) + 0.5 ) ) 
+        h_x = h_x_0;
+        h_y = h_y_0;
+      end
+    end
+    return self.__hyhx2geohex( h_y, h_x, level );
+  end
+
+  private
+  def self.__hyhx2geohex(h_y,h_x,level) 
+    
+    h_x_100 = ( h_x / 3600).floor
+    h_x_10  = ((h_x % 3600) / 60).floor
+    h_x_1   = ((h_x % 3600) % 60).floor
+    h_y_100 = ( h_y / 3600).floor
+    h_y_10  = ((h_y % 3600) / 60).floor
+    h_y_1   = ((h_y % 3600) % 60).floor
+    
+    code = nil
+    if ( level < 7 ) 
+      code = H_KEY[ level % 60 ,1] + H_KEY[ h_x_100 ,1] + H_KEY[ h_y_100,1 ] +
+        H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10,1 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ]
+    elsif ( level == 7 ) 
+      code = H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ];
+    else 
+      code = H_KEY[ level % 60, 1 ] + H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10,1 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ]
+    end
+    return code;
   end
 end
--- spec/geohex_spec.rb
+++ spec/geohex_spec.rb
@@ -12,10 +12,9 @@ describe GeoHex do
     lambda { GeoHex.encode(0,-180,61) }.should raise_error(ArgumentError) # invalid level
   end
   it "should convert coordinates to geohex code" do
-    hex = GeoHex.encode(35.647401,139.716911,1)
-    hex.code == '132KpuG' and 
-    hex.lat == 35.6478085
-    hex.lng == 139.7173629550321
+    GeoHex.encode(35.647401,139.716911,1) == '132KpuG' 
+    #hex.lat == 35.6478085
+    #hex.lng == 139.7173629550321
     
   end
 end
