0a08f51eac3bf0d5b375dc5b05994d53be4128e2
Haruyuki SEKI <hal.marsellus@gmail.com>
2009-11-19 16:08:49 +0900

    porting encode method from kokogiko perl module

--- lib/geohex.rb
+++ lib/geohex.rb
@@ -5,10 +5,10 @@
 class GeoHex
   H_KEY       = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX';
 
-  MIN_X_LON   = 122930; #与那国島
-  MIN_X_LAT   = 24448;
-  MIN_Y_LON   = 141470; ##南硫黄島
-  MIN_Y_LAT   = 24228;
+  MIN_X_LON   = 122930.0; #与那国島
+  MIN_X_LAT   = 24448.0;
+  MIN_Y_LON   = 141470.0; ##南硫黄島
+  MIN_Y_LAT   = 24228.0;
   H_GRID      = 1000;
   H_SIZE      = 0.5;
   
@@ -24,14 +24,14 @@ class GeoHex
     unit_y   = 2.8  * level * H_SIZE
     h_k      = (( (1.4 / 3) * H_GRID).round.to_f / H_GRID)
 
-    base_x   = ( (MIN_X_LON + MIN_X_LAT / h_k      ) / unit_x).floor
-    base_y   = ( (MIN_Y_LAT - h_k      * MIN_Y_LON) / unit_y).floor
+    base_x   = ( (MIN_X_LON + MIN_X_LAT / h_k      ) / unit_x).floor.to_f
+    base_y   = ( (MIN_Y_LAT - h_k      * MIN_Y_LON) / unit_y).floor.to_f
     h_pos_x  = ( lon_grid + lat_grid / h_k     ) / unit_x - base_x
     h_pos_y  = ( lat_grid - h_k      * lon_grid) / unit_y - base_y
     h_x_0    = h_pos_x.floor
     h_y_0    = h_pos_y.floor
-    h_x_q    = ((h_pos_x - h_x_0) * 100).floor / 100
-    h_y_q    = ((h_pos_y - h_y_0) * 100).floor / 100
+    h_x_q    = ((h_pos_x - h_x_0) * 100).floor.to_f / 100
+    h_y_q    = ((h_pos_y - h_y_0) * 100).floor.to_f / 100
     h_x      = h_pos_x.round
     h_y      = h_pos_y.round
 
@@ -53,7 +53,8 @@ class GeoHex
 
   private
   def self.__hyhx2geohex(h_y,h_x,level) 
-    
+    h_x = h_x.to_f
+    h_y = h_y.to_f
     h_x_100 = ( h_x / 3600).floor
     h_x_10  = ((h_x % 3600) / 60).floor
     h_x_1   = ((h_x % 3600) % 60).floor
@@ -62,11 +63,12 @@ class GeoHex
     h_y_1   = ((h_y % 3600) % 60).floor
     
     code = nil
+
     if ( level < 7 ) 
       code = H_KEY[ level % 60 ,1] + H_KEY[ h_x_100 ,1] + H_KEY[ h_y_100,1 ] +
         H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10,1 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ]
     elsif ( level == 7 ) 
-      code = H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ];
+      code = H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10,1 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ]
     else 
       code = H_KEY[ level % 60, 1 ] + H_KEY[ h_x_10,1 ] + H_KEY[ h_y_10,1 ] + H_KEY[ h_x_1,1 ] + H_KEY[ h_y_1,1 ]
     end
--- spec/geohex_spec.rb
+++ spec/geohex_spec.rb
@@ -12,7 +12,16 @@ describe GeoHex do
     lambda { GeoHex.encode(0,-180,61) }.should raise_error(ArgumentError) # invalid level
   end
   it "should convert coordinates to geohex code" do
-    GeoHex.encode(35.647401,139.716911,1) == '132KpuG' 
+    # simple test
+    GeoHex.encode(35.647401,139.716911,1).should == '132KpuG' 
+    GeoHex.encode(24.340565,124.156201,42).should == 'G028k'
+
+    # correct answers (you can obtain this test variables from jsver_test.html )
+    correctdata = '{' + File.open("#{File.dirname(__FILE__)}/testdata.txt").read + '}'
+    eval(correctdata).each_pair do |k,v|
+      GeoHex.encode(v[0],v[1],v[2]).should == k
+    end
+    
     #hex.lat == 35.6478085
     #hex.lng == 139.7173629550321
     
