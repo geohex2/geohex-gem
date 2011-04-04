d981d8526b8038c8afbc6837860939b030249b2a
Tomohiko Himura <eiel.hal@gmail.com>
2011-04-04 18:43:15 +0900

    refactoring spec file for v3

--- spec/geohex_spec.rb
+++ spec/geohex_spec.rb
@@ -4,22 +4,23 @@ include GeoHex
 
 describe GeoHex do
   before(:all) do
-    @test_ll2hex = []
-    @test_hex2ll = []
-    File.open("#{File.expand_path(File.dirname(__FILE__))}/testdata_ll2hex.txt").read.each_line do |l|
+    @testdata_v2_encode = []
+    @testdata_v2_decode = []
+    data_dir = File.expand_path(File.dirname(__FILE__))
+    File.open("#{data_dir}/testdata_v2_encode.txt").read.each_line do |l|
       if l.slice(0,1) != "#"
         d = l.strip.split(',')
-        @test_ll2hex << [d[0].to_f, d[1].to_f, d[2].to_i, d[3]]
+        @testdata_v2_encode << [d[0].to_f, d[1].to_f, d[2].to_i, d[3]]
       end
     end
-    File.open("#{File.expand_path(File.dirname(__FILE__))}/testdata_hex2ll.txt").read.each_line do |l|
+    File.open("#{data_dir}/testdata_v2_decode.txt").read.each_line do |l|
       if l.slice(0,1) != "#"
         d = l.strip.split(',')
-        @test_hex2ll << [d[0],d[1].to_f, d[2].to_f,d[3].to_i]
+        @testdata_v2_decode << [d[0],d[1].to_f, d[2].to_f,d[3].to_i]
       end
     end
-  end  
-  
+  end
+
   it "should throw error if parameters is not valid" do
     lambda { GeoHex::Zone.encode() }.should raise_error(ArgumentError) # no parameters
     lambda { GeoHex::Zone.encode(-86,100,0) }.should raise_error(ArgumentError) # invalid latitude
@@ -29,16 +30,16 @@ describe GeoHex do
     lambda { GeoHex::Zone.encode(0,180,-1) }.should raise_error(ArgumentError) # invalid level
     lambda { GeoHex::Zone.encode(0,-180,25) }.should raise_error(ArgumentError) # invalid level
   end
+
   it "should convert coordinates to geohex code" do
     # correct answers (you can obtain this test variables from jsver_test.html )
-    @test_ll2hex.each do |v|
+    @testdata_v2_encode.each do |v|
       GeoHex::Zone.encode(v[0],v[1],v[2]).should == v[3]
     end
-    
   end
   it "should convert geohex to coordinates " do
     # correct answers (you can obtain this test variables from jsver_test.html )
-    @test_hex2ll.each do |v|
+    @testdata_v2_decode.each do |v|
       GeoHex::Zone.decode(v[0]).should == [v[1],v[2],v[3]]
     end
   end
